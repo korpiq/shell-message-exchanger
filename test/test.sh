380f8789f5ee45f51fee4d325cf2e5de5e540836
Kalle Hallivuori <korpiq@iki.fi>
2013-03-24 18:28:30 +0200

    Spawn commands given as arguments and pass messages between them.

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$BASH_SOURCE")
 
 echo -n 'try to pass message from one shell to another: '
-./../source/installer.exp sender.sh receiver.sh |
+./../source/installer.exp ./sender.sh ./receiver.sh |
     grep -q "got 'a message'" &&
     echo ok ||
     echo fail
